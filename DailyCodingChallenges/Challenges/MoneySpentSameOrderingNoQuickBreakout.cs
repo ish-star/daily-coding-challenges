df4f575b34c6e86992456ca555da331e9012e000
Michael Griehm <mikeg@ish-star.com>
2020-11-11 15:41:38 -0500

    Implementation of several different flavors of the money spent code challenge.
    Turns out the Same Ordering with a Quick Breakout if the price combination is equal to the max budget and a quick break of the inner loop if the price drops below the already calculated max price under the budget is the quickest running, which makes sense since it has all the possible advantages of reducing the amount of combinations that need to be analyzed.

---
+++ DailyCodingChallenges/Challenges/MoneySpentSameOrderingNoQuickBreakout.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace Challenges
+{
+    public class MoneySpentSameOrderingNoQuickBreakout : IMoneySpent
+    {
+        /// <summary>
+        /// A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return .
+        /// </summary>
+        /// <param name="keyboards">the keyboard prices</param>
+        /// <param name="drives">the drive prices</param>
+        /// <param name="b">the budget</param>
+        /// <returns>The Max Spendable amount or -1 if no combinations are within budget.</returns>
+        public int GetMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            var orderedKeyboards = keyboards.OrderByDescending(x => x);
+            var orderedDrives = drives.OrderByDescending(x => x);
+
+            var maxSpendableMoney = -1;
+
+            foreach (var keyboard in orderedKeyboards)
+            {
+                foreach (var drive in orderedDrives)
+                {
+                    var price = keyboard + drive;
+
+                    if (price <= b)
+                    {
+                        if (maxSpendableMoney < price)
+                        {
+                            maxSpendableMoney = price;
+                        }
+                    }
+                }
+            }
+
+            return maxSpendableMoney;
+        }
+    }
+}
