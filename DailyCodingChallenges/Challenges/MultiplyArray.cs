b1a577b102ee3441b7287109942307782789f656
Michael Griehm <mikeg@ish-star.com>
2020-11-11 09:15:57 -0500

    Second Daily Coding Challenge.

---
+++ DailyCodingChallenges/Challenges/MultiplyArray.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenges
+{
+    public class MultiplyArray
+    {
+        //Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+        public static int[] MultiplyNumbersInArrayAndReturnArray(int[] array)
+        {
+            var resultArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                resultArray[i] = 1;
+
+                for (int x = 0; x < array.Length; x++)
+                {
+                    if (i != x)
+                    {
+                        resultArray[i] = resultArray[i] * array[x];
+                    }
+                }
+            }
+
+            return resultArray;
+        }
+    }
+}
---
+++ DailyCodingChallenges/Tests/MultiplyArrayUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges;
+
+namespace Tests
+{
+    [TestClass]
+    public class MultiplyArrayUnitTests
+    {
+        [TestMethod]
+        public void MultiplyNumbersInArrayAndReturnArray_LengthOfThreeArray()
+        {
+            var array = new[] { 3, 2, 1};
+
+            var result = MultiplyArray.MultiplyNumbersInArrayAndReturnArray(array);
+
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(6, result[2]);
+        }
+
+        [TestMethod]
+        public void MultiplyNumbersInArrayAndReturnArray_LengthOfFiveArray()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+
+            var result = MultiplyArray.MultiplyNumbersInArrayAndReturnArray(array);
+
+            Assert.AreEqual(120, result[0]);
+            Assert.AreEqual(60, result[1]);
+            Assert.AreEqual(40, result[2]);
+            Assert.AreEqual(30, result[3]);
+            Assert.AreEqual(24, result[4]);
+        }
+    }
+}
